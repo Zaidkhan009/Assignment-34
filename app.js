7b304011c15aee78bb4f4e8a339d7c90046bd9e9
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-26 01:45:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var favourite_pizza = ["fajita", "Chiken fajita", "Veg"];
+// for (let pizza of favourite_pizza) {
+//     console.log (pizza)
+// }
+// console.log ("\n")
+for (var _i = 0, favourite_pizza_1 = favourite_pizza; _i < favourite_pizza_1.length; _i++) {
+    var pizza = favourite_pizza_1[_i];
+    console.log(" I really like ".concat(pizza, " pizza!"));
+}
+console.log("I really like to eat pizza");
